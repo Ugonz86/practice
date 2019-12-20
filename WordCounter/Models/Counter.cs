4c76edf13a371a840ff2c7e93c05c045e42b8cba
Ugonz86 <ugonzalez86@gmail.com>
2019-12-20 12:31:07 -0800

    Add RepeatCounter class and function. In progress.

--- WordCounter/Models/Counter.cs
+++ WordCounter/Models/Counter.cs
@@ -2,5 +2,16 @@ using System;
 
 namespace WordCounter.Models
 {
-    
+    public class RepeatCounter
+    {
+        public string WordInput { get; set; }
+        public string SentenceInput { get; set; }
+
+        public RepeatCounter(string word, string sentence)
+        {
+            WordInput = word;
+            SentenceInput = sentence;
+        }
+
+    }
 }
